9d63d7eb9967cc7a8b292179a389e815f64f2daf
Jhon Alex <jhon.1620@live.com>
2019-11-09 12:11:24 -0500

    cambio temporal

--- src/routes/web/Home/index.jsx
+++ src/routes/web/Home/index.jsx
@@ -1,9 +1,26 @@
 import React, { Component } from 'react'
 import { Card, Header } from 'semantic-ui-react'
+import axios from 'axios'
 
 class Home extends Component {
   componentDidMount() {
     console.log('idnex')
+  }
+
+  handleS3 = (e) => {
+    const { files } = e.target
+    console.log('file', files)
+    const data = new FormData();
+    data.append('file', files[0]);
+    axios.get('http://localhost:8080/Get_Job/aws/listBuckets')
+      .then(rs => console.log('res', rs))
+      .catch(err => console.log('err', err))
+    // const reader = new FileReader();
+    // reader.readAsDataURL(files[0]);
+    // reader.onload = (e) => {
+    //   console.log(e.target.result)
+    //   axios.ge
+    // }
   }
 
   render() {
@@ -12,34 +29,7 @@ class Home extends Component {
         <Header className="content-title" as="h2">Nuevo HDB</Header>
         <Card>
           <Card.Content>
-            <h1>Home</h1>
-            <h1>Home</h1>
-            <h1>Home</h1>
-            <h1>Home</h1>
-            <h1>Home</h1>
-            <h1>Home</h1>
-            <h1>Home</h1>
-            <h1>Home</h1>
-            <h1>Home</h1>
-            <h1>Home</h1>
-            <h1>Home</h1>
-            <h1>Home</h1>
-            <h1>Home</h1>
-            <h1>Home</h1>
-            <h1>Home</h1>
-            <h1>Home</h1>
-            <h1>Home</h1>
-            <h1>Home</h1>
-            <h1>Home</h1>
-            <h1>Home</h1>
-            <h1>Home</h1>
-            <h1>Home</h1>
-            <h1>Home</h1>
-            <h1>Home</h1>
-            <h1>Home</h1>
-            <h1>Home</h1>
-            <h1>Home</h1>
-            <h1>Home</h1>
+            <input type="file" onChange={this.handleS3} />
           </Card.Content>
         </Card>
         <div className="back" />
